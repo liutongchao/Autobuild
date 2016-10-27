87fcf68b174aed28309969244a69181778b0f9ab
liutongchao <413281269@qq.com>
2016-10-27 17:48:01 +0800

    更改字段 APPNAME 改为 PROJECT_NAME， SCHEME 改为 TAGREAT_NAME

--- AutoBuild/AutoBuildProject/autobuild.py
+++ AutoBuild/AutoBuildProject/autobuild.py
@@ -8,22 +8,22 @@ from optparse import OptionParser
 import subprocess
 
 # 需要改动的地方 (根据自己的项目信息改动改动)
-APPNAME = "AutoBuildProject"  #项目名称
+PROJECT_NAME = "AutoBuildProject"  #项目名称
 VERSION = "1.0.0"  #打包版本号 会根据不同的版本创建文件夹（与项目本身的版本号无关）
 
 CONFIGURATION = "Release"  #Release 环境  Debug 环境
 # CONFIGURATION = "Debug"   
 
-SCHEME = "%s" %(APPNAME) #scheme 就是对应的target
-SCHEME = "AutoBuildProject2"
+TAGREAT_NAME = "%s" %(PROJECT_NAME) #scheme 就是对应的target
+TAGREAT_NAME = "AutoBuildProject2"
 
 PROFILE = "AdHoc" #配置文件分为三种 AdHoc  Dev  AppStore 分别对应三总配置文件
 
 OUTPUT = "./Packge/%s" %(CONFIGURATION)  #打包导出ipa文件路径（请确保 “%s” 之前的文件夹正确并存在）
 
 
-WORKSPACE = "%s.xcworkspace" %(APPNAME)
-PROJECT = "%s.xcodeproj" %(APPNAME)
+WORKSPACE = "%s.xcworkspace" %(PROJECT_NAME)
+PROJECT = "%s.xcodeproj" %(PROJECT_NAME)
 
 #如果在项目中没有用到 pod 请注释掉此行
 # PROJECT = None
@@ -77,7 +77,7 @@ def uploadPgy(ipaPath):
 #打包project
 def buildProject(project, scheme, output):
 
-    buildCmd = 'xcodebuild archive -project %s -scheme %s -sdk %s -configuration %s  ONLY_ACTIVE_ARCH=NO -archivePath ./build/%s.xcarchive' %(project, scheme, SDK, CONFIGURATION,APPNAME)
+    buildCmd = 'xcodebuild archive -project %s -scheme %s -sdk %s -configuration %s  ONLY_ACTIVE_ARCH=NO -archivePath ./build/%s.xcarchive' %(project, scheme, SDK, CONFIGURATION,PROJECT_NAME)
     process = subprocess.Popen(buildCmd, shell = True)
     process.wait()
     print "*************************buildProject*******************************\n %s \n********************************************************" %(buildCmd)
@@ -85,12 +85,12 @@ def buildProject(project, scheme, output):
     createDir(OUTPUT)
     createDir(OUTPUT+"/"+VERSION)
 
-    signCmd = 'xcodebuild -exportArchive -archivePath ./build/%s.xcarchive -exportPath %s/%s/%s_%s_%s -exportOptionsPlist ./AutoBuild/plist/%s.plist' %(APPNAME, output, VERSION,APPNAME,VERSION,CONFIGURATION,PROFILE)
+    signCmd = 'xcodebuild -exportArchive -archivePath ./build/%s.xcarchive -exportPath %s/%s/%s_%s_%s -exportOptionsPlist ./AutoBuild/plist/%s.plist' %(PROJECT_NAME, output, VERSION,PROJECT_NAME,VERSION,CONFIGURATION,PROFILE)
     process = subprocess.Popen(signCmd, shell = True)
     process.wait()
     print "*************************signCmd*******************************\n %s \n********************************************************" %(signCmd)
     
-    ipaPath = "%s/%s/%s_%s_%s/%s.ipa" %(output, VERSION,APPNAME,VERSION,CONFIGURATION,SCHEME)
+    ipaPath = "%s/%s/%s_%s_%s/%s.ipa" %(output, VERSION,PROJECT_NAME,VERSION,CONFIGURATION,TAGREAT_NAME)
 
     if OPEN_PYUPLOAD == True:
         uploadPgy(ipaPath)
@@ -100,7 +100,7 @@ def buildProject(project, scheme, output):
 #打包workspace
 def buildWorkspace(workspace, scheme, output):
 
-    buildCmd = 'xcodebuild archive -workspace %s -scheme %s -sdk %s -configuration %s  ONLY_ACTIVE_ARCH=NO -archivePath ./build/%s.xcarchive' %(workspace, scheme, SDK, CONFIGURATION, APPNAME)
+    buildCmd = 'xcodebuild archive -workspace %s -scheme %s -sdk %s -configuration %s  ONLY_ACTIVE_ARCH=NO -archivePath ./build/%s.xcarchive' %(workspace, scheme, SDK, CONFIGURATION, PROJECT_NAME)
     process = subprocess.Popen(buildCmd, shell = True)
     process.wait()
     print "*************************buildWorkspace*******************************\n %s \n********************************************************" %(buildCmd)
@@ -108,12 +108,12 @@ def buildWorkspace(workspace, scheme, output):
     createDir(OUTPUT)
     createDir(OUTPUT+"/"+VERSION)
 
-    signCmd = 'xcodebuild -exportArchive -archivePath ./build/%s.xcarchive -exportPath %s/%s/%s_%s_%s -exportOptionsPlist ./AutoBuild/plist/%s.plist' %(APPNAME, output, VERSION,APPNAME,VERSION,CONFIGURATION,PROFILE)
+    signCmd = 'xcodebuild -exportArchive -archivePath ./build/%s.xcarchive -exportPath %s/%s/%s_%s_%s -exportOptionsPlist ./AutoBuild/plist/%s.plist' %(PROJECT_NAME, output, VERSION,PROJECT_NAME,VERSION,CONFIGURATION,PROFILE)
     process = subprocess.Popen(signCmd, shell = True)
     process.wait()
     print "*************************signCmd*******************************\n %s \n********************************************************" %(signCmd)
 
-    ipaPath = "%s/%s/%s_%s_%s/%s.ipa" %(output, VERSION,APPNAME,VERSION,CONFIGURATION,SCHEME)
+    ipaPath = "%s/%s/%s_%s_%s/%s.ipa" %(output, VERSION,PROJECT_NAME,VERSION,CONFIGURATION,TAGREAT_NAME)
 
     if OPEN_PYUPLOAD == True:
         uploadPgy(ipaPath)
@@ -146,7 +146,7 @@ def main():
     parser = OptionParser()
     parser.add_option("-w", "--workspace", default=WORKSPACE, help="Build the workspace test.xcworkspace.")
     parser.add_option("-p", "--project", default=PROJECT, help="Build the project test.xcodeproj.")
-    parser.add_option("-s", "--scheme", default=SCHEME, help="Build the scheme specified by schemename. Required if building a workspace.")
+    parser.add_option("-s", "--scheme", default=TAGREAT_NAME, help="Build the scheme specified by schemename. Required if building a workspace.")
     parser.add_option("-o", "--output", default=OUTPUT, help="specify output filename")
 
     (options, args) = parser.parse_args()
